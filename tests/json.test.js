a35e7646ce32cdb914787d59cda3e83e17a6146b
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-09-20 23:48:40 -0400

    chore: test

--- tests/json.test.js
+++ tests/json.test.js
@@ -54,9 +54,10 @@ test('Check if JSON file follows format', () => {
 test('Check if JSON file matches the value name', () => {
   const passed = getFileName(process.env.FILES, data);
 
+  console.log(process.env.FILES);
+
   let fileNameMessage = passed === true ? "File name matches target.RECORD_TYPE." : "File name does not match any target.RECORD_TYPE.";
 
   core.setOutput('fileNameMessage', fileNameMessage);
-  expect(typeof passed).toBe('boolean'); // Check if the result is a boolean
   expect(passed).toBe(true);
 });
